f8752c48fc19fa13037ea8b515568c0d3441b5ff
mmuneeb1000 <muneebhashmi8@gmail.com>
2026-06-20 16:59:50 +0500

    countdown functionality defined

--- client/src/components/Countdown.jsx
+++ client/src/components/Countdown.jsx
@@ -1,25 +1,73 @@
+import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 
 function Countdown() {
+  const [timeLeft, setTimeLeft] =
+    useState <
+    TimeLeft >
+    {
+      days: 8,
+      hours: 23,
+      minutes: 55,
+      seconds: 41,
+    };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((prev) => {
+        let { days, hours, minutes, seconds } = prev;
+
+        if (seconds > 0) {
+          seconds -= 1;
+        } else if (minutes > 0) {
+          minutes -= 1;
+          seconds = 59;
+        } else if (hours > 0) {
+          hours -= 1;
+          minutes = 59;
+          seconds = 59;
+        } else if (days > 0) {
+          days = -1;
+          hours = 23;
+          minutes = 59;
+          seconds = 59;
+        } else {
+          clearInterval(timer);
+        }
+        return { days, hours, minutes, seconds };
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <section>
+    <section class="countdown-container">
       <h1>We're Launching Soon</h1>
       <div className="countdown">
-        <span className="days-count"></span>
-        <p>Days</p>
+        <span className="time-count">
+          {String(timeLeft.days).padStart(2, "0")}
+        </span>
+        <p className="time-label">Days</p>
       </div>
       <div className="countdown">
-        <span className="hours-count"></span>
-        <p>Hours</p>
+        <span className="time-count">
+          {String(timeLeft.hours).padStart(2, "0")}
+        </span>
+        <p className="time-label">Hours</p>
       </div>
       <div className="countdown">
-        <span className="minutes-count"></span>
-        <p>Minutes</p>
+        <span className="time-count">
+          {String(timeLeft.minutes).padStart(2, "0")}
+        </span>
+        <p className="time-label">Minutes</p>
       </div>
       <div className="countdown">
-        <span className="seconds-count"></span>
-        <p>Seconds</p>
+        <span className="time-count">
+          {String(timeLeft.seconds).padStart(2, "0")}
+        </span>
+        <p className="time-label">Seconds</p>
       </div>
     </section>
   );
