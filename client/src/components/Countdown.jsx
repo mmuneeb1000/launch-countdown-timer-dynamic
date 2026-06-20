7a73c7923b83862a131a3594950e6ba2e801a36e
mmuneeb1000 <muneebhashmi8@gmail.com>
2026-06-20 16:41:10 +0500

    components established

--- client/src/components/Countdown.jsx
+++ client/src/components/Countdown.jsx
@@ -1,3 +1,28 @@
-export default function Countdown() {
-  return <h1>this is countdown</h1>;
+import { useState } from "react";
+import { useEffect } from "react";
+
+function Countdown() {
+  return (
+    <section>
+      <h1>We're Launching Soon</h1>
+      <div className="countdown">
+        <span className="days-count"></span>
+        <p>Days</p>
+      </div>
+      <div className="countdown">
+        <span className="hours-count"></span>
+        <p>Hours</p>
+      </div>
+      <div className="countdown">
+        <span className="minutes-count"></span>
+        <p>Minutes</p>
+      </div>
+      <div className="countdown">
+        <span className="seconds-count"></span>
+        <p>Seconds</p>
+      </div>
+    </section>
+  );
 }
+
+export default Countdown;
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,13 @@
+import facebookIcon from "../assets/images/icon-facebook.svg";
+import InstagramIcon from "../assets/images/icon-instagram.svg";
+import pinterestIcon from "../assets/images/icon-pinterest.svg";
+
 export default function Footer() {
-  return <h1>This is footer</h1>;
+  return (
+    <footer>
+      <img src={facebookIcon} />
+      <img src={InstagramIcon} />
+      <img src={pinterestIcon} />
+    </footer>
+  );
 }
